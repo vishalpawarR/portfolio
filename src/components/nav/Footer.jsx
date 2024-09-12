0b11933239ff06c0a41d1e152208a78b1da60967
Vishal Pawar <vishalpawarr.git@gmail.com>
2024-09-12 23:21:04 +0530

    feat: Built the footer

--- src/components/nav/Footer.jsx
+++ src/components/nav/Footer.jsx
@@ -1,5 +1,38 @@
+import { PhoneIcon } from '@heroicons/react/24/solid'
+
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className='flex justify-center items-center h-16'>
+      <div>
+        <a className='w-10 h-10 grid place-items-center rounded-md hover:bg-[#495057]' href="tel:+91-7411334439" target="_blank">
+          <PhoneIcon className='w-5' />
+        </a>
+      </div>
+      <div>
+        <a className='w-10 h-10 grid place-items-center rounded-md hover:bg-[#495057]' href="https://wa.me/+917411334439" target="_blank">
+          <img src='./icons/whatsapp.svg' alt=''  className='w-5' />
+        </a>
+      </div>
+      <div>
+        <a className='w-10 h-10 grid place-items-center rounded-md hover:bg-[#495057]' href="mailto:vishalpawarr.git@gmail.com" target="_blank">
+          <img src='./icons/gmail.svg' alt=''  className='w-5' />
+        </a>
+      </div>
+      <div>
+        <a className='w-10 h-10 grid place-items-center rounded-md hover:bg-[#495057]' href="https://x.com/VishalPawar_R" target="_blank">
+          <img src='./icons/x.svg' alt=''  className='w-5' />
+        </a>
+      </div>
+      <div>
+        <a className='w-10 h-10 grid place-items-center rounded-md hover:bg-[#495057]' href="https://www.linkedin.com/in/vishalpawarr/" target="_blank">
+          <img src='./icons/linkedin.svg' alt=''  className='w-5' />
+        </a>
+      </div>
+      <div>
+        <a className='w-10 h-10 grid place-items-center rounded-md hover:bg-[#495057]' href="https://github.com/vishalpawarR/" target="_blank">
+          <img src='./icons/github.svg' alt=''  className='w-5' />
+        </a>
+      </div>
+    </footer>
   )
 }
