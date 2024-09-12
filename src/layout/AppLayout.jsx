ddf49670a0f777fc940c542ddad850d811ada00e
Vishal Pawar <vishalpawarr.git@gmail.com>
2024-09-12 16:27:31 +0530

    refactor: removed the Nav from layout

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -8,7 +8,7 @@ export default function AppLayout() {
         <div className='flex flex-col min-h-screen'>
             <Header />
             <main className='flex-grow max-w-3xl mx-auto px-3'>
-                <Nav />
+                {/* <Nav /> */}
                 <Outlet />
             </main>
             <Footer />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import Nav from "../components/nav/Nav";
 import Tweet from "../components/Tweet";
 
 export default function Home() {
     return (
         <div className="">
+            <Nav />
             <Tweet />
         </div>
     )
