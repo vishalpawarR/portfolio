47e7fcdfdf6c78a2ab9bf114784a6a7cf9f9ebda
Vishal Pawar <vishalpawarr.git@gmail.com>
2024-09-12 23:37:58 +0530

    chore: added the indexing to logo

--- src/components/nav/Header.jsx
+++ src/components/nav/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="h-16 flex items-center">
         <nav className='flex max-w-screen-xl mx-auto px-6 items-center'>
             <h1 className="text-2xl font-bold text-white">
-              <NavLink>&lt;VP /&gt;</NavLink>
+              <NavLink to="/">&lt;VP /&gt;</NavLink>
             </h1>
         </nav>
     </header>
