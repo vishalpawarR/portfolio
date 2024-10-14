3220f1b52064be840f52dc760df4f452024cc1a2
Vishal Pawar <vishalpawarr.git@gmail.com>
2024-10-14 22:37:11 +0530

    feat: fixed the SEO issue of meta tag

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -9,7 +9,7 @@ export default function AppLayout() {
       <MetaTags
         title="Vishal Pawar | ReactJS Developer"
         description="Passionate React.js Developer with over two years of experience in crafting interactive user interfaces and optimizing web applications. Skilled in JavaScript, TypeScript, and SQL, with expertise in React.js, Next.js, and GraphQL. Proven track record in enhancing performance, mentoring junior developers, and collaborating with cross-functional teams to deliver seamless user experiences. Committed to implementing responsive design and best practices in modern web development."
-        image="../assets/react.svg"
+        image="../../assets/react.svg"
         url="https://vishalpawar.netlify.app/"
       />
       <Header />
