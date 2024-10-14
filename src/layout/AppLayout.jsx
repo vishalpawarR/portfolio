01754a9e6a5cf0aff3140acd46e1babca0b3aa2b
Vishal Pawar <vishalpawarr.git@gmail.com>
2024-10-14 22:17:25 +0530

    chore: removed unwanted nav

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,17 +1,21 @@
-import { Outlet } from 'react-router-dom'
-import Header from '../components/nav/Header'
-import Footer from '../components/nav/Footer'
-import Nav from '../components/nav/Nav'
+import { Outlet } from "react-router-dom"
+import Header from "../components/nav/Header"
+import Footer from "../components/nav/Footer"
 
 export default function AppLayout() {
-    return (
-        <div className='flex flex-col min-h-screen'>
-            <Header />
-            <main className='flex-grow max-w-3xl mx-auto px-3'>
-                {/* <Nav /> */}
-                <Outlet />
-            </main>
-            <Footer />
-        </div>
-    )
+  return (
+    <div className="flex flex-col min-h-screen">
+        title="Vishal Pawar | ReactJS Developer"
+        description="Passionate React.js Developer with over two years of experience in crafting interactive user interfaces and optimizing web applications. Skilled in JavaScript, TypeScript, and SQL, with expertise in React.js, Next.js, and GraphQL. Proven track record in enhancing performance, mentoring junior developers, and collaborating with cross-functional teams to deliver seamless user experiences. Committed to implementing responsive design and best practices in modern web development."
+        image="../assets/react.svg"
+        url="https://vishalpawar.netlify.app/"
+      />
+      <Header />
+      <main className="flex-grow max-w-3xl mx-auto px-3">
+        {/* <Nav /> */}
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  )
 }
