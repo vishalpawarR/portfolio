64a34680351c468cb7156e60d0a8a8d4e51f40b8
Vishal Pawar <vishalpawarr.git@gmail.com>
2024-10-14 22:56:15 +0530

    chore: fixed the SEO meta description

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -7,9 +7,9 @@ export default function AppLayout() {
   return (
     <div className="flex flex-col min-h-screen">
       <MetaTags
-        title="Vishal Pawar | ReactJS Developer"
-        description="Passionate React.js Developer with over three years of experience in crafting interactive user interfaces and optimizing web applications. Skilled in JavaScript, TypeScript, and SQL, with expertise in React.js, Next.js, and GraphQL. Proven track record in enhancing performance, mentoring junior developers, and collaborating with cross-functional teams to deliver seamless user experiences. Committed to implementing responsive design and best practices in modern web development."
-        image="https://vishalpawar.netlify.app/assets/react.svg"
+        title="Vishal Pawar | Software Engineer"
+        description="Vishal Pawar is a versatile Software Engineer with over three years of experience in developing innovative web applications across various technologies. Proficient in JavaScript, TypeScript, and SQL, he possesses strong knowledge of React.js and Next.js, alongside expertise in GraphQL and other modern web technologies. With a proven track record of optimizing performance, mentoring junior developers, and collaborating with cross-functional teams, Vishal is dedicated to delivering seamless user experiences. He is committed to implementing responsive design and adhering to best practices in software development."
+        image="https://vishalpawar.netlify.app/react.svg"
         url="https://vishalpawar.netlify.app/"
       />
       <Header />
