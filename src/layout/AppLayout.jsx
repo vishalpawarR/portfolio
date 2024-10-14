cc35d1a1290388fc4e26b9b6d6713ad9a011c417
Vishal Pawar <vishalpawarr.git@gmail.com>
2024-10-14 22:43:52 +0530

    bug: fixed the SEO image url issue

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -8,8 +8,8 @@ export default function AppLayout() {
     <div className="flex flex-col min-h-screen">
       <MetaTags
         title="Vishal Pawar | ReactJS Developer"
-        description="Passionate React.js Developer with over two years of experience in crafting interactive user interfaces and optimizing web applications. Skilled in JavaScript, TypeScript, and SQL, with expertise in React.js, Next.js, and GraphQL. Proven track record in enhancing performance, mentoring junior developers, and collaborating with cross-functional teams to deliver seamless user experiences. Committed to implementing responsive design and best practices in modern web development."
-        image="../../assets/react.svg"
+        description="Passionate React.js Developer with over three years of experience in crafting interactive user interfaces and optimizing web applications. Skilled in JavaScript, TypeScript, and SQL, with expertise in React.js, Next.js, and GraphQL. Proven track record in enhancing performance, mentoring junior developers, and collaborating with cross-functional teams to deliver seamless user experiences. Committed to implementing responsive design and best practices in modern web development."
+        image="https://vishalpawar.netlify.app/assets/react.svg"
         url="https://vishalpawar.netlify.app/"
       />
       <Header />
