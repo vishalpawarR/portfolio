4d934aaf063c93a0a083b3a6027560f9f44c03ed
Vishal Pawar <vishalpawarr.git@gmail.com>
2024-10-19 21:03:57 +0530

    feat: updated the 404 page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import MetaTags from "../components/MetaTags"
+import { Link } from "react-router-dom"
 
 export default function PageNotFound() {
   return (
-    <div>PageNotFound</div>
+    <>
+      <div className="text-center">
         <MetaTags title="404 - Vishal Pawar" />
+        <div className="flex justify-center items-center">
+          <h1 className="text-xl">
+            Whoops! This page is missing. 🚷💔 Time to explore the homepage? 🏡
+          </h1>
+        </div>
+        <p className=" m-4 text-blue-400 font-bold text-center">
+          <Link
+            to="/"
+            className="hover:underline"
+          >
+            {" "}
+            <span className="hover:text-white">/go to homepage</span>
+          </Link>
+        </p>
+      </div>
+    </>
   )
 }
