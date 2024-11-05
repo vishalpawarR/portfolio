f30c1d39a1b9efa690dd59cf401c5404c8a7ee7f
Vishal Pawar <vishalpawarr.git@gmail.com>
2024-11-06 01:09:38 +0530

    feat: added backword compatiable links

--- src/route/router.jsx
+++ src/route/router.jsx
@@ -23,6 +23,150 @@ export const router = createBrowserRouter([
         element: <RedirectWithUTM />,
       },
       {
+        path: "direct-apply",
+        element: (
+          <Navigate
+            to="/?utm_source=direct&utm_medium=direct_application&utm_campaign=job_application&utm_content=job_listing"
+            replace
+          />
+        ),
+      },
+      {
+        path: "hire-resume",
+        element: (
+          <Navigate
+            to="/?utm_source=resume&utm_medium=document&utm_campaign=job_application&utm_content=cv_link"
+            replace
+          />
+        ),
+      },
+      {
+        path: "hire-email",
+        element: (
+          <Navigate
+            to="/?utm_source=email&utm_medium=direct&utm_campaign=job_application&utm_content=recruiter"
+            replace
+          />
+        ),
+      },
+      {
+        path: "hire-linkedin",
+        element: (
+          <Navigate
+            to="/?utm_source=linkedin&utm_medium=social&utm_campaign=job_application&utm_content=linkedin_message"
+            replace
+          />
+        ),
+      },
+      {
+        path: "hire-whatsapp",
+        element: (
+          <Navigate
+            to="/?utm_source=whatsapp&utm_medium=social&utm_campaign=job_application&utm_content=personal_message"
+            replace
+          />
+        ),
+      },
+      {
+        path: "hire-naukri",
+        element: (
+          <Navigate
+            to="/?utm_source=naukri&utm_medium=job_board&utm_campaign=job_application&utm_content=job_listing"
+            replace
+          />
+        ),
+      },
+      {
+        path: "hire-wellfound",
+        element: (
+          <Navigate
+            to="/?utm_source=wellfound&utm_medium=job_board&utm_campaign=job_application&utm_content=job_listing"
+            replace
+          />
+        ),
+      },
+      {
+        path: "hire-weekday",
+        element: (
+          <Navigate
+            to="/?utm_source=weekday&utm_medium=job_board&utm_campaign=job_application&utm_content=job_listing"
+            replace
+          />
+        ),
+      },
+      {
+        path: "hire-yc",
+        element: (
+          <Navigate
+            to="/?utm_source=ycombinator&utm_medium=job_board&utm_campaign=job_application&utm_content=job_listing"
+            replace
+          />
+        ),
+      },
+      {
+        path: "hire-uplers",
+        element: (
+          <Navigate
+            to="/?utm_source=uplers&utm_medium=job_board&utm_campaign=job_application&utm_content=job_listing"
+            replace
+          />
+        ),
+      },
+      {
+        path: "hire-crossover",
+        element: (
+          <Navigate
+            to="/?utm_source=crossover&utm_medium=job_board&utm_campaign=job_application&utm_content=job_listing"
+            replace
+          />
+        ),
+      },
+      {
+        path: "hire-flexiple",
+        element: (
+          <Navigate
+            to="/?utm_source=flexiple&utm_medium=job_board&utm_campaign=job_application&utm_content=job_listing"
+            replace
+          />
+        ),
+      },
+      {
+        path: "hire-hirist",
+        element: (
+          <Navigate
+            to="/?utm_source=hirist&utm_medium=job_board&utm_campaign=job_application&utm_content=job_listing"
+            replace
+          />
+        ),
+      },
+      {
+        path: "hire-cuvette",
+        element: (
+          <Navigate
+            to="/?utm_source=cuvette&utm_medium=job_board&utm_campaign=job_application&utm_content=job_listing"
+            replace
+          />
+        ),
+      },
+      {
+        path: "hire-github",
+        element: (
+          <Navigate
+            to="/?utm_source=github&utm_medium=github_profile&utm_campaign=lets_connect&utm_content=readme_connect"
+            replace
+          />
+        ),
+      },
+      {
+        path: "hire-reactflux",
+        element: (
+          <Navigate
+            to="/?utm_source=reactflux&utm_medium=community&utm_campaign=job_application&utm_content=job_listing"
+            replace
+          />
+        ),
+      },
+      {
         path: "*",
         element: <PageNotFound />,
       },
